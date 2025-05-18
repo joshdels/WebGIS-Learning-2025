9cb990837dc242cae34bdf8560edb131bc1ae5fb
joshdels <deleonjoshy2@gmail.com>
2025-05-18 10:56:06 +0800

    updates

---
+++ WebGIS Pro/Day 1/distance.js
@@ -0,0 +1,17 @@
+// ASSIGNMENT
+let x1 = 1;
+let y1 = 1;
+let x2 = 2;
+let y2 = 2;
+
+let distancePythagoras = 
+  Math.sqrt(
+    Math.pow((x2-x1), 2) + 
+    Math.pow((y2-y1), 2)
+  )
+let sqrtOfNum = Math.sqrt(x1);
+let roundedNum = sqrtOfNum.toFixed(2);
+
+console.log(sqrtOfNum);
+console.log(distancePythagoras);
+console.log(roundedNum);
---
+++ WebGIS Pro/Day 1/number.js
@@ -0,0 +1,7 @@
+// DEMO
+let myNumber = 5;
+let sqrtOfNumber = Math.sqrt(myNumber);
+let roundedNumber = sqrtOfNumber.toFixed(3);
+console.log(sqrtOfNumber);
+console.log(roundedNumber);
+
