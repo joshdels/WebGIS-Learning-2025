282b39888bcc542d5a7ee4f603e43993242b7565
joshdels <deleonjoshy2@gmail.com>
2025-05-19 22:39:36 +0800

    live server

---
+++ WebGIS Pro/Day 2 - String Exercise/string.js
@@ -0,0 +1,5 @@
+//Exercise
+let point = ' some other sfdd text POINT(36.099796, -112.11213 94) some other ch....';
+let startPosPoint = point.indexOf("POINT(");
+console.log(startPosPoint);
+console.log("Hey");
