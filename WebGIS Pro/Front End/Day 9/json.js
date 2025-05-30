b38c6cc0d76688422f41a4b27ec4dcae95f7e9ac
joshdels <deleonjoshy2@gmail.com>
2025-05-30 22:07:39 +0800

    josn done

---
+++ WebGIS Pro/Front End/Day 9/json.js
@@ -0,0 +1,13 @@
+let jsonData = {
+  type: "Feature",
+  properties: {
+    name: "Coor",
+    amenity: "Baseball",
+  },
+  geometry: {
+    type: "Point",
+    coordinates: [-104.1234, 39.12341],
+  },
+};
+
+console.log();
