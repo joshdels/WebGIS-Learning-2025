47c3f1740c583a0a3fa7a027daa0c3e315194ec4
joshdels <deleonjoshy2@gmail.com>
2025-06-01 11:41:55 +0800

    done day 11

---
+++ WebGIS Pro/Front End/Day 11/js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('greetingButton').addEventListener("click", function(){
+  let username = document.getElementById('username').value;
+  alert("Hello, " + username +"!");
+});
