3c0a104ed0b9780b09e0347f6a3a12a5450735f5
joshdels <deleonjoshy2@gmail.com>
2025-06-24 20:36:33 +0800

    done practice django+ postgis connection

--- WebGIS Pro Academy/Back End/webgisproject/webgisproject/settings.py
+++ WebGIS Pro Academy/Back End/webgisproject/webgisproject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+# ADD  GDAL
+GDAL_LIBRARY_PATH = r"C:\Users\deleo\miniconda3\envs\webgisbackend\Library\bin\gdal"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'webgisapplication'
+    'django.contrib.gis',
+    'webgisapplication',
+
 ]
 
 MIDDLEWARE = [
@@ -75,11 +80,14 @@ WSGI_APPLICATION = "webgisproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": 'geodjango', #database
+        'USER': 'postgres', #username
+        'PASSWORD': 'joshua765', #pw
+        'HOST': 'localhost', #localhost
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
