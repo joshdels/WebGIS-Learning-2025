bc71341731fa91f07b5e3b923c068f4f80542fa3
joshdels <deleonjoshy2@gmail.com>
2025-07-17 22:05:58 +0800

    upload to django and leaflet hehe

---
+++ WebGIS Pro Academy/Back End/searchplace02/locationfinder/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+  template = loader.get_template('leaflet.html')
+  return HttpResponse(template.render())
+
