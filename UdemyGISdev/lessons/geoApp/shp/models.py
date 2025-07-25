6a36aca6ef69afcca4d052debdbd5e02ff888f92
joshdels <deleonjoshy2@gmail.com>
2025-07-25 17:31:51 +0800

    updated

--- UdemyGISdev/lessons/geoApp/shp/models.py
+++ UdemyGISdev/lessons/geoApp/shp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 import datetime
+from django.core.signals import post_save, post_delete
+from django.dispatch import receiver
+import geopandas as gpd
+import os
+from sqlalchemy import *
+from geoalchemy2 import Geometry, WKTElement
 
-# Create your models here.
+
+# The shp model
 class Shp(models.Model):
   name = models.CharField(max_length=50)
   description = models.CharField(max_length=200, blank=True)
@@ -10,3 +17,11 @@ class Shp(models.Model):
 
   def __str__(self):
     return self.name
+  
+@receiver(post_save, sender=Shp)
+def published_data(sender, instance, created, **kwargs):
+  pass
+  
+@receiver(post_delete, sender=Shp)
+def delete_data(sender, instance, created, **kwargs):
+  pass
